a95031c14b90e83a435934559a1a59a7ab651e64
aoyagikouhei <aoyagi.kouhei@gmail.com>
2025-11-18 10:13:27 +0900

    made sql builder

--- apps/services/sample_builder/src/main.rs
+++ apps/services/sample_builder/src/main.rs
@@ -1,5 +1,14 @@
 use sql_query_builder as sql;
+use sqlx::prelude::*;
 use sqlx::postgres::PgPoolOptions;
+use uuid::Uuid;
+
+#[allow(dead_code)]
+#[derive(FromRow, Debug)]
+struct User {
+    pub uuid: Uuid,
+    pub user_name: String,
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -10,20 +19,21 @@ async fn main() -> anyhow::Result<()> {
         .connect("postgres://user:pass@postgresql/web")
         .await?;
 
-    let mut select = sql::Select::new()
-        .select("id, login")
+    let select = sql::Select::new()
+        .select("uuid, user_name")
         .from("users")
-        .where_clause("login = $1");
-
-    let is_admin = true;
-
-    if is_admin {
-        select = select.where_clause("is_admin = true");
-    }
+        ;
 
     let query = select.as_string();
 
     println!("{query}");
+
+    let row: User = sqlx::query_as(&query)
+//        .bind(uuid)
+        .fetch_one(&pool)
+        .await?;
+
+    println!("{:?}", row);
 
     //api::execute()?;
     Ok(())
